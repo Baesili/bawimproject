c219bd9a5cf889a7698ca567468ebe9928b617be
BW-AdapTech <bw.adaptech@gmail.com>
2022-12-28 21:05:17 +0100

    Dodano system logowania

--- cookie_shop/logout.php
+++ cookie_shop/logout.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if(isset($_SESSION['username'])
+if(isset($_SESSION['username']))
 	unset($_SESSION['username']);
-if(isset($_SESSION['type'])
+if(isset($_SESSION['type']))
 	unset($_SESSION['type']);
 if (isset($_SERVER["HTTP_REFERER"])) {
 	header("Location: " . $_SERVER["HTTP_REFERER"]);
